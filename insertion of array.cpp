74c82fa9bad78010d25c83b005493e992ece0ac2
Joshithaaguru <joshithaaguru@gmail.com>
2025-03-12 19:35:49 +0530

    Add files via upload

---
+++ insertion of array.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,value,pos;
+	printf("enter the number of elements");
+	scanf("%d",&n);
+	int arr[n+1];
+	printf("enter %d elements \n",n);
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	printf("enter the element to insert:");
+	scanf("%d",&value);
+	printf("enter the position:");
+	scanf("%d",&pos);
+	for(i=n;i>=pos;i--){
+		arr[i]=arr[i-1];
+	}
+	arr[pos-1]=value;
+	n++;
+	printf("after insetion:\n");
+	for(i=0;i<n;i++){
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+	return 0;
+}
